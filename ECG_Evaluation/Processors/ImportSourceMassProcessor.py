be684998a5d9d0ddc711753ce880a6652d4f594a
Huy Quang Dinh <qhi1903@gmail.com>
2022-05-27 13:23:41 +0700

    Update the Mass Import for the SciPy library if no required information is defined.

--- ECG_Evaluation/Processors/ImportSourceMassProcessor.py
+++ ECG_Evaluation/Processors/ImportSourceMassProcessor.py
@@ -12,14 +12,38 @@ import Controllers.WFDBHelper as wfdb_helper
 
 class ImportSourceMassProcessor:
     def save_ecg_property_masss(self, db_result, dir_name, file_list:list[Files], format_desc, list_ecg_attributes):
+        # Check source, which must be defined
+        source = list_ecg_attributes[cons.ECG_SOURCE]
+        if not source:
+            st.warning('Source must be defined!')
+            return
+        
         db= db_result[cons.DB_NAME]
         ecg_col= db_result[cons.COLLECTION_ECG_NAME]
 
+        # Process each record from the folder
         for ecg_record in file_list:
+            # Access and read the ECG record property
             ecg_property:ECG = wfdb_helper.read_property(dir_name, ecg_record.file_path, ecg_record.file_name,format_desc.lower())
             # Update 'sample' property by total number of samples instead of data signal before saving ECG property
             # It can cause an error of 'the BSON document too large'
             ecg_property.sample = len(ecg_property.sample)
+
+            # Check if the sample_rate is NOT defined, use the given sample rate from UI
+            if not ecg_property.sample_rate:
+                fs = list_ecg_attributes[cons.ECG_SAMPLE_RATE]
+                ecg_property.sample_rate = fs
+                ecg_property.time =  round(ecg_property.sample / fs)
+
+            # Check if the unit is NOT defined, use the given unit from UI
+            if not ecg_property.unit:
+                ecg_property.unit = list_ecg_attributes[cons.ECG_UNIT]
+
+            # Set values for source and comments from UI to import into DB
+            ecg_property.source = source
+            ecg_property.comments = list_ecg_attributes[cons.ECG_COMMENTS]
+
+            # Save the ECG property to MongoDB
             ecg_id = scraper.save_ecg_property(ecg_col, ecg_property)
             if ecg_id:
                 list_file_id = []
@@ -33,7 +57,7 @@ class ImportSourceMassProcessor:
                         ecg_id=ecg_id
                         )
 
-                    # Save ECG file to MongoDB
+                    # Save ECG files to MongoDB
                     file_id = scraper.save_ecg_file(db, file_metadata, ecg_property)
                     if file_id:
                         list_file_id.append([file_id])
--- ECG_Evaluation/Views/ImportSourceMassView.py
+++ ECG_Evaluation/Views/ImportSourceMassView.py
@@ -15,16 +15,18 @@ def load_form():
 def render_property():
     result = None
     with st.form("import_source_mass_form"):
+        source = st.text_input('Source')
+        comments = st.text_area('Comments', 'Add any comments here', height=120)
         mass_import_guideline = '<p style="font-family:Source Sans Pro, sans-serif; color:orange; font-size: 15px;">Please note that \'Mass Import\' will use the following attributes for all records if any of them is missing.</p>'
         st.markdown(mass_import_guideline, unsafe_allow_html=True)
         sample_rate = st.number_input('Sample rate',min_value=500,step=1)
         unit = st.selectbox('Unit', options=[cons.CONS_UNIT_MV, cons.CONS_UNIT_V])
-        comments = st.text_area('Comments', 'Add any comments here', height=120)
         
         # Every form must have a submit button.
         submitted = st.form_submit_button("Mass Import")
         if submitted:
             result = {
+                cons.ECG_SOURCE: source,
                 cons.ECG_SAMPLE_RATE: sample_rate,
                 cons.ECG_UNIT: unit,
                 cons.ECG_COMMENTS: comments
